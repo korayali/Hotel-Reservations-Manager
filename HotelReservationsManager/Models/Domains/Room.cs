e3c9c46c92511ed2982cb4724336b6f4791a7aa5
m10ev <mtenev@outlook.com>
2026-03-21 16:57:27 +0200

    fix: add one more null!

--- HotelReservationsManager/Models/Domains/Room.cs
+++ HotelReservationsManager/Models/Domains/Room.cs
@@ -3,7 +3,7 @@
     public class Room : BaseEntity
     {
         public int Capacity { get; set; }
-        public string Type { get; set; }
+        public string Type { get; set; } = null!;
         public bool IsFree { get; set; } = true;
         public double PricePerAdult { get; set; }
         public double PricePerChild { get; set; }
