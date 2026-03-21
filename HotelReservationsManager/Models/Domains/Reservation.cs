5065ac9ae9691fdc4786038659ca355c98fd8cd5
korayali <korayali8209@gmail.com>
2026-03-21 16:25:17 +0200

    Edit in Reservation

--- HotelReservationsManager/Models/Domains/Reservation.cs
+++ HotelReservationsManager/Models/Domains/Reservation.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int RoomId { get; set; }
         public int GuestId { get; set; }
-        public List<Guests> GuestList { get; set; }
+        public List<Guest> GuestList { get; set; }
         public DateTime CheckInDate { get; set; }
         public DateTime CheckOutDate { get; set; }
         public bool HasBreakfast { get; set; }
