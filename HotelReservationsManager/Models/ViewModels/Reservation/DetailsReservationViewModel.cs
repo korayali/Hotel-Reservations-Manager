718c09faba3fc920b67b26c60142b8b9874cb6c6
korayali <korayali8209@gmail.com>
2026-03-22 14:17:45 +0200

    Added Details View Models

---
+++ HotelReservationsManager/Models/ViewModels/Reservation/DetailsReservationViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationsManager.Models.ViewModels.Reservation
+{
+    public class DetailsReservationViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Room ID")]
+        public int RoomId { get; set; }
+
+        [Required]
+        [Display(Name = "User ID")]
+        public string UserId { get; set; } = null!;
+
+
+        [Required]
+        [Display(Name = "Check-in Date")]
+        public DateTime CheckInDate { get; set; }
+
+
+        [Required]
+        [Display(Name = "Check-out Date")]
+        public DateTime CheckOutDate { get; set; }
+
+
+        [Required]
+        [Display(Name = "Breakfast")]
+        public bool HasBreakfast { get; set; }
+
+
+        [Required]
+        [Display(Name = "All-Inclusive")]
+        public bool IsAllInclusive { get; set; }
+
+
+        [Required]
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ HotelReservationsManager/Models/ViewModels/Room/DetailsRoomViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationsManager.Models.ViewModels.Room
+{
+    public class DetailsRoomViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Capacity")]
+        public int Capacity { get; set; }
+
+        [Required]
+        [Display(Name = "Type")]
+        public string Type { get; set; } = null!;
+
+        [Display(Name = "Available")]
+        public bool IsFree { get; set; } = true;
+
+        [Required]
+        [Display(Name = "Price Per Adult")]
+        public double PricePerAdult { get; set; }
+
+        [Required]
+        [Display(Name = "Price Per Child")]
+        public double PricePerChild { get; set; }
+
+        [Required]
+        [Display(Name = "Room Number")]
+        public int RoomNumber { get; set; }
+
+
+    }
+}
