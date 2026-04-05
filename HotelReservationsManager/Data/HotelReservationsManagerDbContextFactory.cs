6372894ce046935e29772a0bf720207b07c76b70
m10ev <mtenev@outlook.com>
2026-04-05 12:26:01 +0300

    fix: add context factory because it fails on older servers

---
+++ HotelReservationsManager/Data/HotelReservationsManagerDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HotelReservationsManager.Data
+{
+    public class HotelReservationsManagerDbContextFactory : IDesignTimeDbContextFactory<HotelReservationsManagerDbContext>
+    {
+        public HotelReservationsManagerDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelReservationsManagerDbContext>();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new HotelReservationsManagerDbContext(optionsBuilder.Options);
+        }
+    }
+}
